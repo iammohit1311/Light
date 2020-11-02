10fc25820132b52c8664b3398b286b02be102708
Mohit Srivastava <iammohitpro@gmail.com>
2020-11-02 22:57:03 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+    const id = socket.handshake.query.id
+    socket.join(id)
+
+    socket.on('send-message', ({ recipients, text }) => {
+        recipients.forEach(recipient => {
+            const newRecipients = recipients.filter(r => r !== recipient)
+            newRecipients.push(id)
+            socket.broadcast.to(recipient).emit('receive-message', {
+                recipients: newRecipients, sender: id, text
+            })
+        })
+    })
+})
+
+server.listen(process.env.PORT||3030)
